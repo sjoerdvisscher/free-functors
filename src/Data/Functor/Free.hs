272b46567b35e3c68613ca09001a4d0f11de567a
Sjoerd Visscher <sjoerd@w3future.com>
2013-08-26 17:25:00 +0200

    More documentation

--- src/Data/Functor/Free.hs
+++ src/Data/Functor/Free.hs
@@ -28,6 +28,7 @@ module Data.Functor.Free where
   
 import Control.Applicative
 import Control.Comonad
+import Data.Function
 
 import Data.Constraint hiding (Class)
 import Data.Constraint.Forall
@@ -42,9 +43,18 @@ import Data.Algebra
 import Data.Algebra.TH
 import Language.Haskell.TH.Syntax
 
--- | The free functor for constraint @c@.
+-- | The free functor for class @c@. 
+--
+--   @Free c a@ is basically an expression tree with operations from class @c@ 
+--   and variables/placeholders of type @a@, created with `unit`.
+--   Monadic bind allows you to replace each of these variables with another sub-expression.
 newtype Free c a = Free { runFree :: forall b. c b => (a -> b) -> b }
 
+-- | Derive the instances for the class @c@ of @`Free` c a@ and @`LiftAFree` c f a@.
+--
+-- For example: 
+-- 
+-- @deriveInstances ''Num@
 deriveInstances :: Name -> Q [Dec]
 deriveInstances nm = concat <$> sequenceA
   [ deriveSignature nm
@@ -62,9 +72,11 @@ deriveInstances nm = concat <$> sequenceA
     a = mkName "a"
     f = mkName "f"
   
+-- | `unit` allows you to create `Free c` values, together with the operations from the class @c@.
 unit :: a -> Free c a
 unit a = Free $ \k -> k a
 
+-- | `rightAdjunct` is the destructor of `Free c` values.
 rightAdjunct :: c b => (a -> b) -> Free c a -> b
 rightAdjunct f g = runFree g f
 
@@ -94,14 +106,27 @@ counit = rightAdjunct id
 leftAdjunct :: (Free c a -> b) -> a -> b
 leftAdjunct f = f . unit
 
--- | @transform f . transform g = transform (g . f)@
+-- | @transform f as = as >>= f unit@
+--
+-- @transform f . transform g = transform (g . f)@
 transform :: (forall r. c r => (b -> r) -> a -> r) -> Free c a -> Free c b
 transform t (Free f) = Free (f . t)
 
-unfold :: (b -> Free c (Either b a)) -> b -> Free c a
-unfold g = go 
-  where 
-    go = transform (\f -> either (rightAdjunct f . go) f) . g
+-- | @unfold f = coproduct (unfold f) unit . f@
+--
+-- `inL` and `inR` are useful here. For example, the following creates the list @[1..10]@ as a @Free Monoid@:
+--
+-- @unfold (\b -> if b == 0 then mempty else `inL` (b - 1) \<> `inR` b) 10@
+unfold :: (b -> Coproduct c b a) -> b -> Free c a
+unfold f = fix $ \go -> transform (\k -> either (rightAdjunct k . go) k) . f
+
+-- | @convert = rightAdjunct pure@
+convert :: (c (f a), Applicative f) => Free c a -> f a
+convert = rightAdjunct pure
+
+-- | @convertClosed = rightAdjunct absurd@
+convertClosed :: c r => Free c Void -> r
+convertClosed = rightAdjunct absurd
 
 instance Functor (Free c) where
   fmap f = transform (. f)
@@ -131,13 +156,8 @@ instance ForallT c (LiftAFree c) => Foldable (Free c) where
   foldMap = foldMapDefault
 
 instance ForallT c (LiftAFree c) => Traversable (Free c) where
-  traverse f = getLiftAFree . rightAdjunctT (LiftAFree . fmap pure . f)
+  traverse f = getLiftAFree . rightAdjunctT (LiftAFree . fmap unit . f)
 
-convert :: (c (f a), Applicative f) => Free c a -> f a
-convert = rightAdjunct pure
-
-convertClosed :: c r => Free c Void -> r
-convertClosed = rightAdjunct absurd
 
 
 -- * Coproducts
